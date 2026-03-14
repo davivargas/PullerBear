287c4988924b0f06c2ccf8b55f61538cf6e7475c
Li-Yang-CS <kavenli23@gmail.com>
2026-03-14 16:41:06 -0700

    modularized the code, added function to write summary obj to local json, changed way it compares context

---
+++ src/gitTools/types.ts
@@ -0,0 +1,36 @@
+import * as vscode from 'vscode';
+import { ExplainerViewProvider, CommitSummary } from '../ExplainerViewProvider';
+
+export interface RepoMonitorState
+{
+    commitTimestamps : number[];
+    lastBehindCount  : number;
+    intervalHandle?  : NodeJS.Timeout;
+    isChecking       : boolean;
+}
+
+export interface GitMonitorConfig
+{
+    commitWindowMinutes      : number;
+    hardStopCommitThreshold : number;
+    warningCommitThreshold  : number;
+    fetchIntervalMinutes     : number;
+}
+
+export interface RepositoryContext
+{
+    repository : any;
+    state      : RepoMonitorState;
+    config     : GitMonitorConfig;
+}
+
+export interface CheckResult
+{
+    shouldContinue : boolean;
+    behindCount    : number;
+    newCommits     : number;
+}
+
+export { CommitSummary };
+export { ExplainerViewProvider };
+export { vscode };
