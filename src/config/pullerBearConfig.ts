c268a721c5c9499a79676c34fe72808e17329f77
LEGACY21\tkngu <tknguyen2111@gmail.com>
2026-03-15 08:15:55 -0700

    remove apikey from config file

--- src/config/pullerBearConfig.ts
+++ src/config/pullerBearConfig.ts
@@ -19,6 +19,6 @@ export function getPullerBearConfig(): PullerBearConfig
         warningCommitThreshold  : config.get<number>('warningCommitThreshold', 2),
         hardStopCommitThreshold : config.get<number>('hardStopCommitThreshold', 5),
         branchRef               : config.get<string>('branchRef', 'main'),
-        apiKey                  : config.get<string>('apiKey', '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')
+        apiKey                  : config.get<string>('apiKey', '')
     };
 }
